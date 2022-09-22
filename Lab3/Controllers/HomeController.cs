ed60abbf8c036907e72ef5e992ed4b4c9cc615ee
Ali Abbiss <aliabbiss@rocketmortgage.com>
2022-09-22 19:34:51 -0400

    lab 3 duplicated ready for work

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterUser()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessRegistration(string fname, string lname, string email, string password)
+        {
+            ViewBag.fname = fname;
+            ViewBag.lname = lname;
+            ViewBag.email = email;
+            ViewBag.password = password;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
